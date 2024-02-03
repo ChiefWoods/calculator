6ff5dab5987674f219b9562d8c475946cda27d3b
Chii Yuen <chii.yuen@hotmail.com>
2024-02-03 20:55:08 +0800

    Reformat code

--- script.js
+++ script.js
@@ -27,36 +27,31 @@ function resetDisplay() {
 }
 
 function backspaceDisplay() {
-  shouldClear
-    ? clearScreen()
-    : shouldReset
-      ? resetDisplay()
-      : null;
+  if (shouldClear) clearScreen();
+  if (shouldReset) resetDisplay();
+
   display.textContent = display.textContent.length === 1
     ? '0'
     : display.textContent.slice(0, -1);
 }
 
 function appendNumber(number) {
-  shouldClear
-    ? clearScreen()
-    : shouldReset
-      ? resetDisplay()
-      : null;
+  if (shouldClear) clearScreen();
+  if (shouldReset) resetDisplay();
+
   if (display.textContent.length > 9) {
     return alert('Maximum digit limit reached!');
   }
+
   display.textContent === '0'
     ? display.textContent = number
     : display.textContent += number;
 }
 
 function appendDecimal() {
-  shouldClear
-    ? clearScreen()
-    : shouldReset
-      ? resetDisplay()
-      : null;
+  if (shouldClear) clearScreen();
+  if (shouldReset) resetDisplay();
+
   if (!display.textContent.includes('.')) {
     display.textContent += '.';
   }
@@ -72,35 +67,28 @@ function toggleNegative() {
     mode = '';
     shouldClear = false;
   }
+
   if (display.textContent === '0') return;
-  display.textContent.startsWith('-')
-    ? display.textContent = display.textContent.slice(1)
-    : display.textContent = '-' + display.textContent;
+
+  display.textContent = display.textContent.startsWith('-')
+    ? display.textContent.slice(1)
+    : '-' + display.textContent;
 }
 
 function trimDisplay() {
   let num = display.textContent;
+
   if (num === '') {
     num = '0';
   }
-  return parseFloat(num).toString();
-}
 
-function roundAndExponent(num) {
-  if (num.toString().length > 9) {
-    if (/e\+0/.test(num.toExponential(4))) {
-      return num.toExponential(4).slice(0, -3);
-    } else {
-      return num.toExponential(4);
-    }
-  } else {
-    return num;
-  }
+  return parseFloat(num).toString();
 }
 
 function operate(num1, num2, operator) {
   num1 = Number(num1);
   num2 = Number(num2);
+
   let num = operator === '+'
     ? num1 + num2
     : operator === '−'
@@ -110,7 +98,12 @@ function operate(num1, num2, operator) {
         : operator === '÷'
           ? num1 / num2
           : num1;
-  return roundAndExponent(num);
+
+  num = Math.round(num * 10000) / 10000;
+
+  return num.toString().length > 9
+    ? num.toExponential(4)
+    : num;
 }
 
 function setMode(operator) {
@@ -125,50 +118,62 @@ function setMode(operator) {
 
 function evaluate() {
   if (display.textContent === 'AND BEYOND!') return;
-  if (mode === '=') { // repeated evaluation without operating
+
+  if (mode === '=') {
+    // repeated evaluation without operating
     mode = '';
     operand1 = trimDisplay();
     shouldReset = true;
-  } else if (shouldClear) { // repeated evaluation
+  } else if (shouldClear) {
+    // repeated evaluation
     operand1 = trimDisplay();
   } else {
     operand2 = trimDisplay();
     shouldClear = true;
-    if (mode === '÷' && operand2 === '0') { // dividing by zero
+    if (mode === '÷' && operand2 === '0') {
+      // dividing by zero
       input.textContent = 'TO INFINITY';
       display.textContent = 'AND BEYOND!';
       return;
     }
   }
+
   input.textContent = `${operand1} ${mode} ${operand2} = `;
   display.textContent = operate(operand1, operand2, mode);
 }
 
 function setDisplay(operation) {
   if (display.textContent === 'AND BEYOND!') return;
-  if (mode !== '' && shouldReset) { // select operation after clicking another one
+
+  if (mode !== '' && shouldReset) {
+    // select operation after choosing another one
     operand1 = trimDisplay();
-  } else if (mode !== '' && shouldClear) { // start an operation using evaluated result
+  } else if (mode !== '' && shouldClear) {
+    // start an operation using evaluated result
     shouldClear = false;
     shouldReset = true;
     operand1 = trimDisplay();
-  } else if (mode !== '' && !shouldClear) { // append new operation
+  } else if (mode !== '' && !shouldClear) {
+    // append new operation
     operand2 = trimDisplay();
-    if (mode === '÷' && operand2 === '0') { // dividing by zero
+    if (mode === '÷' && operand2 === '0') {
+      // dividing by zero
       shouldClear = true;
       input.textContent = 'TO INFINITY';
       display.textContent = 'AND BEYOND!';
       return;
     } else {
       shouldReset = true;
-      setMode(operation);
       display.textContent = operate(operand1, operand2, mode);
+      setMode(operation);
       operand1 = trimDisplay();
     }
-  } else if (mode === '') { // first operation
+  } else if (mode === '') {
+    // first operation
     shouldReset = true;
     operand1 = trimDisplay();
   }
+
   setMode(operation);
   input.textContent = `${operand1} ${mode}`;
 }
@@ -197,6 +202,7 @@ digitBtns.forEach(digit => {
 
 window.addEventListener('keydown', e => {
   const button = document.querySelector(`[data-key*='${e.key}']`);
+
   if (button) {
     button.classList.add('active');
     ['n', 'N'].includes(e.key)
@@ -221,6 +227,7 @@ window.addEventListener('keydown', e => {
 
 window.addEventListener('keyup', e => {
   const button = document.querySelector(`[data-key*='${e.key}']`);
+
   if (button) {
     button.classList.remove('active');
   }
